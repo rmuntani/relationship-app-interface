85b83eb86a9e2a4da6cca1b11622c8529622fcb4
Raphael Montani <raphael.muntani@gmail.com>
2019-08-11 18:38:40 -0300

    Fix MatchModal tests

--- tests/components/MatchModal.test.jsx
+++ tests/components/MatchModal.test.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { cleanup, render } from '@testing-library/react';
+import { cleanup, render, fireEvent } from '@testing-library/react';
 import MatchModal from '../../src/components/MatchModal';
 
 describe('MatchModal', () => {
   afterEach(cleanup);
 
   const matchData = {
-    id: 1,
-    age: 75,
-    name: 'Paulo Freire',
-    image: {
-      alt: 'It\'s me, Paulo',
-      src: 'paulo.jpg',
+    closeModal: jest.fn(),
+    user: {
+      age: 75,
+      name: 'Paulo Freire',
+      image: {
+        alt: 'It\'s me, Paulo',
+        src: 'paulo.jpg',
+      },
     },
   };
 
@@ -25,13 +27,27 @@ describe('MatchModal', () => {
   });
 
   it('should render alt regardless of lack of alt', () => {
-    const imageWithoutAlt = { image: { src: 'paulo.jpg' } };
-    const dataWithoutAlt = { ...matchData, ...imageWithoutAlt };
-    const { container, getByText } = render(<MatchModal {...dataWithoutAlt} />);
+    const matchWithoutImage = {...matchData};
+    matchWithoutImage.user.image = { src: 'paulo.jpg' };
+
+    const { container, getByText } = render(<MatchModal {...matchWithoutImage} />);
     const image = container.querySelector('img[src=\'paulo.jpg\']');
 
     getByText(/Paulo Freire/);
     expect(image.src).toMatch(/paulo.jpg/);
     expect(image.alt).toMatch(/Paulo Freire/);
+  });
+
+  it('should call closeModal function when it\'s clicked or a key is pressed', () => {
+    const { container } = render(<MatchModal {...matchData} />);
+    const image = container.querySelector('img[src=\'paulo.jpg\']');
+
+    fireEvent.click(image);
+
+    expect(matchData.closeModal).toHaveBeenCalledTimes(1);
+
+    fireEvent.keyDown(image, { key: 'ArrowLeft', keyCode: 37, which: 37 });
+
+    expect(matchData.closeModal).toHaveBeenCalledTimes(2);
   });
 });
